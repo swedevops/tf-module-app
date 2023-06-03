ae8d642e979325574a90c4f3c16d6490105c6a0a
swedev99 <swedevops99@gmail.com>
2023-06-04 00:42:36 +0530

    code dry

--- main.tf
+++ main.tf
@@ -1,53 +1,51 @@
-#resource "aws_security_group" "sg" {
-#  name        = "${var.name}-${var.env}-sg"
-#  description = "${var.name}-${var.env}-sg"
-#  vpc_id      = var.vpc_id
-#
-#  ingress {
-#    description      = "APP"
-#    from_port        = 8080
-#    to_port          = 8080
-#    protocol         = "tcp"
-#    cidr_blocks      = [aws_vpc.main.cidr_block]
-#
-#  }
-#
-#  egress {
-#    from_port        = 0
-#    to_port          = 0
-#    protocol         = "-1"
-#    cidr_blocks      = ["0.0.0.0/0"]
-#    ipv6_cidr_blocks = ["::/0"]
-#  }
-#
-#  tags = {
-#    Name = "allow_tls"
-#  }
-#}
-#
-#resource "aws_launch_template" "template" {
-#  name_prefix   = "${var.name}-${var.env}"
-#  image_id      = data.aws_ami.ami.id
-#  instance_type = var.instance_type
-#}
-#
-#resource "aws_autoscaling_group" "bar" {
-#  availability_zones = ["us-east-1a"]
-#  desired_capacity   = 1
-#  max_size           = 1
-#  min_size           = 1
-#
-#  launch_template {
-#    id      = aws_launch_template.foobar.id
-#    version = "$Latest"
-#  }
-#}
-resource "aws_instance" "test" {
-  ami = "ami-08ee87f57b38db5af"
-  instance_type = var.instance_type
-  subnet_id = var.subnet_id
+resource "aws_security_group" "sg" {
+  name        = "${var.name}-${var.env}-sg"
+  description = "${var.name}-${var.env}-sg"
+  vpc_id      = var.vpc_id
 
+  ingress {
+    description      = "APP"
+    from_port        = var.app_port
+    to_port          = var.app_port
+    protocol         = "tcp"
+    cidr_blocks      = var.allow_app_cidr
+
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.bastion_cidr
+  }
+
+  tags = {
+    Name = "${var.name}-${var.env}-sg"
+  }
 }
 
-variable "instance_type" {}
-variable "subnet_id" {}
+resource "aws_launch_template" "template" {
+  name_prefix   = "${var.name}-${var.env}-lt"
+  image_id      = data.aws_ami.ami.id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.sg.id]
+}
+
+resource "aws_autoscaling_group" "asg" {
+  name               = "${var.name}-${var.env}-asg"
+  desired_capacity   = var.desired_capacity
+  max_size           = var.max_size
+  min_size           = var.min_size
+  vpc_zone_identifier = var.subnet_ids
+
+  launch_template {
+    id      = aws_launch_template.template.id
+    version = "$Latest"
+  }
+}
+
+
+
+
+
---
+++ vars.tf
@@ -0,0 +1,11 @@
+variable "instance_type" {}
+variable "subnet_ids" {}
+variable "name" {}
+variable "env" {}
+variable "vpc_id" {}
+variable "allow_app_cidr" {}
+variable "bastion_cidr" {}
+variable "app_port" {}
+variable "desired_capacity" {}
+variable "max_size" {}
+variable "min_size" {}
