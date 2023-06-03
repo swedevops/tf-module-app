5b61175200b2f78bf2ec93271d2ea5191904de21
swedev99 <swedevops99@gmail.com>
2023-06-04 01:11:20 +0530

    code dry

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
-output "subnets" {
-  value = module.subnets
-}
-output "vpc_id"{
-  value = aws_vpc.main.id
-}
+#output "subnets" {
+#  value = module.subnets
+#}
+#output "vpc_id"{
+#  value = aws_vpc.main.id
+#}
