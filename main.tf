c8f388f6d5aa7f459ddbb4b03f04b211a1faf0d7
swedev99 <swedevops99@gmail.com>
2023-06-04 01:02:38 +0530

    code dry

--- main.tf
+++ main.tf
@@ -5,8 +5,8 @@ resource "aws_security_group" "sg" {
 
   ingress {
     description      = "APP"
-    from_port        = var.app_port
-    to_port          = var.app_port
+    from_port        = 8080
+    to_port          = 8080
     protocol         = "tcp"
     cidr_blocks      = var.allow_app_cidr
 
