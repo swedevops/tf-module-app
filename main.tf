dc206078d9648584be725092bb30cd9e8cc69caf
swedev99 <swedevops99@gmail.com>
2023-06-03 22:50:40 +0530

    code dry

--- main.tf
+++ main.tf
@@ -1,7 +1,50 @@
+#resource "aws_security_group" "sg" {
+#  name        = "${var.name}-${var.env}-sg"
+#  description = "${var.name}-${var.env}-sg"
+#  vpc_id      = var.vpc_id
+#
+#  ingress {
+#    description      = "APP"
+#    from_port        = 8080
+#    to_port          = 8080
+#    protocol         = "tcp"
+#    cidr_blocks      = [aws_vpc.main.cidr_block]
+#
+#  }
+#
+#  egress {
+#    from_port        = 0
+#    to_port          = 0
+#    protocol         = "-1"
+#    cidr_blocks      = ["0.0.0.0/0"]
+#    ipv6_cidr_blocks = ["::/0"]
+#  }
+#
+#  tags = {
+#    Name = "allow_tls"
+#  }
+#}
+#
+#resource "aws_launch_template" "template" {
+#  name_prefix   = "${var.name}-${var.env}"
+#  image_id      = data.aws_ami.ami.id
+#  instance_type = var.instance_type
+#}
+#
+#resource "aws_autoscaling_group" "bar" {
+#  availability_zones = ["us-east-1a"]
+#  desired_capacity   = 1
+#  max_size           = 1
+#  min_size           = 1
+#
+#  launch_template {
+#    id      = aws_launch_template.foobar.id
+#    version = "$Latest"
+#  }
+#}
+
 resource "aws_instance" "test" {
   ami = "ami-08ee87f57b38db5af"
   instance_type = var.instance_type
-  subnet_id = var.subnet_id
+  subnet_id = var.subnet_ids
 }
-variable "instance_type" {}
-variable "subnet_id" {}
---
+++ vars.tf
@@ -0,0 +1,6 @@
+variable "instance_type" {}
+variable "subnet_ids" {}
+variable "name" {}
+variable "env" {}
+variable "vpc_id" {}
+variable "allow_app_cidr" {}
