547ce577432fd85920d2e5aec11949927d0f31db
swedev99 <swedevops99@gmail.com>
2023-06-04 01:07:55 +0530

    code dry

--- vars.tf
+++ vars.tf
@@ -5,7 +5,6 @@ variable "env" {}
 variable "vpc_id" {}
 variable "allow_app_cidr" {}
 variable "bastion_cidr" {}
-variable "app_port" {}
 variable "desired_capacity" {}
 variable "max_size" {}
 variable "min_size" {}
