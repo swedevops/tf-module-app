25fc17dc77374a02a333423a9f8c5a710e742321
swedev99 <swedevops99@gmail.com>
2023-06-06 19:13:16 +0530

    code dry

--- main.tf
+++ main.tf
@@ -54,6 +54,14 @@ resource "aws_autoscaling_group" "asg" {
   }
 }
 
+resource "aws_lb_target_group" "main" {
+  name     = "${var.name}-${var.env}-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+  tags = merge(var.tags, { Name = "${var.name}-${var.env}-tg" })
+}
+
 
 
 
