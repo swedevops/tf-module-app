ea8d9ab37eb65710332623a0801346baca2c0e79
swedev99 <swedevops99@gmail.com>
2023-06-08 19:50:35 +0530

    code dry

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 labauto ansible
-ansible-pull -i localhost, -U https://github.com/raghudevopsb72/roboshop-ansible roboshop.yml -e role_name=${name} -e env=${env} &>>/opt/ansible.log
+ansible-pull -i localhost, -U https://github.com/swedevops/roboshop-ansible roboshop.yml -e role_name=${name} -e env=${env} &>>/opt/ansible.log
